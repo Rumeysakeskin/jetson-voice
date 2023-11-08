4d62f629fa04c8c463c6c53bafffee74bf7bb57f
Rumeysa Keskin <61134521+Rumeysakeskin@users.noreply.github.com>
2023-11-08 13:01:25 +0300

    Update run.sh

--- docker/run.sh
+++ docker/run.sh
@@ -178,7 +178,7 @@ $USER_VOLUME"
 
 if [ $ARCH = "aarch64" ]; then
 
-	sudo docker run --runtime nvidia -it --rm --gpus all \
+	sudo docker run --runtime nvidia --gpus all -it --rm \
 		--name=$CONTAINER_NAME \
 		--network host \
                  --env DISPLAY \
